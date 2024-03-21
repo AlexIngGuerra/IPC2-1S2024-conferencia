f0253a8b76ad4bd5d4e39d018059a03291c74a46
Walter Alexander Guerra Duque <3018766650101@ingenieria.usac.edu.gt>
2024-03-21 12:00:32 -0600

    Modificado generacion de grafica

--- util/dot/Grafica.py
+++ util/dot/Grafica.py
@@ -1,17 +1,20 @@
-
+import subprocess
 
 def generar(contenido, salida):
     try:
-        # Definir ruta dot
-        ruta_dot = "./libs/graphviz/bin/dot"
+        # Definimos la ruta completa al ejecutable dot
+        ruta_dot = "./lib/graphviz/bin/dot"
 
-        # Definimos ruta de archivo dot
-        archivo_dot = "./util/dot/grafica.dot"
+        # Definimos las rutas completas de los archivos
+        archivodot = "./util/dot/grafica.dot"
 
-        # Creamos el archivo
-        with open(archivo_dot, "w") as file:
+        # Creamos y escribimos en el archivo
+        with open(archivodot, "w") as file:
             file.write(contenido)
 
-            
+        # Construimos el comando y lo ejecutamos
+        comando = [ruta_dot, "-Tsvg", archivodot, "-o", salida]
+        subprocess.run(comando, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
     except Exception as e:
         print(e)
